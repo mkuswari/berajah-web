5b64e8ca742de7a63771c2b143931099255920df
Muhammad Kuswari <muh.kuswari10@gmail.com>
2021-01-30 09:14:47 +0800

    add icon if enrolled course empty

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -18,21 +18,28 @@
             <h3 class="font-weight-bold">Kelas yang saya ambil</h3>
             <hr>
             <div class="row">
-                @foreach ($enrollments as $enrollment)
+                @forelse ($enrollments as $enrollment)
                     <div class="col-sm-3">
                         <div class="card shadow border-0 mt-3">
                             <img src="{{ asset('storage/' . $enrollment->courses->thumbnail) }}" class="course-thumbnail"
                                 width="100%">
                             <div class="card-body">
                                 <h6 class="card-title">
-                                    <a href="{{ route('play', [$enrollment->courses->slug]) }}"
-                                        style="text-decoration: none;"
+                                    <a href="{{ route('play', [$enrollment->courses->slug]) }}" style="text-decoration: none;"
                                         class="text-dark">{{ \Str::limit($enrollment->courses->name, 50, '...') }}</a>
                                 </h6>
                             </div>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <div class="row py-5">
+                        <div class="col-sm-8 mx-auto">
+                            <img src="{{ asset('global/images/svg/empty.svg') }}" width="100%">
+                            <h4 class="text-center mt-4 text-muted font-weight-bold">Upps.. Kamu belum punya kelas
+                            </h4>
+                        </div>
+                    </div>
+                @endforelse
             </div>
         </div>
     </section>
